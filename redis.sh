9d05fdf7772a2e2d0eb4fa6a720e847c7fffc133
Your Name <vemasani.pavani@gmail.com>
2025-11-19 01:01:30 +0530

    dns for user,cart,payment,shipping

--- redis.sh
+++ redis.sh
@@ -3,7 +3,9 @@ dnf module enable redis:7 -y
 
 dnf install redis -y
 sed -i 's/127.0.0.1/0.0.0.0' /etc/redis/redis.conf
-sed -i 's/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+sed -i  '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
 
 systemctl enable redis
 systemctl restart redis
+
+
