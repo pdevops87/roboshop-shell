d1929456160da31d4447933915c9d3426bdcac79
Your Name <vemasani.pavani@gmail.com>
2025-11-19 02:00:30 +0530

    dns for user,cart,payment,shipping

--- user.sh
+++ user.sh
@@ -3,13 +3,15 @@ dnf module enable nodejs:20 -y
 
 dnf install nodejs -y
 useradd roboshop
+cp user.service  /etc/systemd/system/user.service
 rm -rf /app
 mkdir /app
 
 curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip
-cp user.service  /etc/systemd/system/user.service
+
 cd /app
 unzip /tmp/user.zip
+
 cd /app
 npm install
 
