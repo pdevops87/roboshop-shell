0b47a55878bf7c3c0bb28b5f33b3c1a1ca349615
Your Name <vemasani.pavani@gmail.com>
2025-11-19 00:51:14 +0530

    dns for user,cart,payment,shipping

--- cart.sh
+++ cart.sh
@@ -4,7 +4,7 @@ dnf module enable nodejs:20 -y
 dnf install nodejs -y
 
 useradd roboshop
-
+rm -rf /app
 mkdir /app
 curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip
 cp cart.service /etc/systemd/system/cart.service
