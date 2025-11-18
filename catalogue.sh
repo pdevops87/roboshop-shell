a20053d5ba22ee1f895d57d00613a2ace602aad4
Your Name <vemasani.pavani@gmail.com>
2025-11-18 23:48:19 +0530

    nodejs dependencies missing

--- catalogue.sh
+++ catalogue.sh
@@ -2,14 +2,18 @@ dnf module disable nodejs -y
 dnf module enable nodejs:20 -y
 dnf install nodejs -y
 useradd roboshop
+rm -rf /app
 mkdir /app
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip
 cp catalogue.service /etc/systemd/system/catalogue.service
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 cd /app
 unzip /tmp/catalogue.zip
+npm install
+
 dnf install mongodb-mongosh -y
 mongosh --host mongodb-dev.pdevops78.online </app/db/master-data.js
+
 systemctl daemon-reload
 systemctl enable catalogue
 systemctl restart catalogue
---
+++ user.sh
@@ -0,0 +1,15 @@
+dnf module disable nodejs -y
+dnf module enable nodejs:20 -y
+
+dnf install nodejs -y
+useradd roboshop
+mkdir /app
+
+curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip
+cp user.service  /etc/systemd/system/user.service
+cd /app
+unzip /tmp/user.zip
+
+cd /app
+npm install
+
