c4a306c3f77ce5cc3821823d3849224348d6d72c
Your Name <vemasani.pavani@gmail.com>
2025-11-26 09:22:57 +0530

    pass a component dynamically

--- catalogue.sh
+++ catalogue.sh
@@ -1,21 +1,45 @@
-dnf module disable nodejs -y
-dnf module enable nodejs:20 -y
+echo -e "\e[32mDisable nodejs module"
+echo -e "\e[35m*****************************\e[0m"
+dnf module disable nodejs -y >>/tmp/roboshop.log
 
-dnf install nodejs -y
-useradd roboshop
+echo -e "\e[32menable nodejs 20 module"
+echo -e "\e[35m*****************************\e[0m"
+dnf module enable nodejs:20 -y >>/tmp/roboshop.log
 
-rm -rf /app
-mkdir /app
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip
-cp catalogue.service /etc/systemd/system/catalogue.service
-cp mongo.repo /etc/yum.repos.d/mongo.repo
-cd /app
-unzip /tmp/catalogue.zip
-npm install
 
-dnf install mongodb-mongosh -y
-mongosh --host mongodb-dev.pdevops78.online </app/db/master-data.js
+echo -e "\e[32minstall nodejs module"
+echo -e "\e[35m*****************************\e[0m"
+dnf install nodejs -y >>/tmp/roboshop.log
+echo $?
 
-systemctl daemon-reload
-systemctl enable catalogue
-systemctl restart catalogue
+echo -e "\e[32mcreate  new user"
+echo -e "\e[35m*****************************\e[0m"
+useradd roboshop  &>>/tmp/roboshop.log
+echo $?
+
+echo -e "\e[32mremove app directory"
+echo -e "\e[35m*****************************\e[0m"
+rm -rf /app >>/tmp/roboshop.log
+echo $?
+
+echo -e "\e[32m make a directory"
+echo -e "\e[35m*****************************\e[0m"
+mkdir /app >>/tmp/roboshop.log
+echo $?
+
+echo -e "\e[32m Download catalogue content"
+echo -e "\e[35m*****************************\e[0m"
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>/tmp/roboshop.log
+echo $?
+
+echo -e "\e[32m navigate to app directory"
+echo -e "\e[35m*****************************\e[0m"
+cd /app >>/tmp/roboshop.log
+echo $?
+
+echo -e "\e[32munzip catalogue content"
+echo -e "\e[35m*****************************\e[0m"
+unzip /tmp/catalogue.zip >>/tmp/roboshop.log
+echo $?
+
+
